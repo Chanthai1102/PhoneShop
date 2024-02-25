820f7b4b70fd8c074ca6a760dbe159c50c4dd2f3
chanthai thy <chanthaithy.bs2021@gmail.com>
2024-02-25 09:22:23 +0700

    add MapStructure

--- src/main/java/com/chanthai/phoneshop/mapper/BrandMapper.java
+++ src/main/java/com/chanthai/phoneshop/mapper/BrandMapper.java
@@ -3,9 +3,13 @@ package com.chanthai.phoneshop.mapper;
 import com.chanthai.phoneshop.dto.BrandDTO;
 import com.chanthai.phoneshop.entity.Brand;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface BrandMapper {
+    BrandMapper INSTANCE = Mappers.getMapper(BrandMapper.class);
+
     Brand toBrand(BrandDTO brandDTO);
+    BrandDTO toBrandDTO(Brand entity);
 
 }
--- src/main/java/com/chanthai/phoneshop/repository/BrandRepository.java
+++ src/main/java/com/chanthai/phoneshop/repository/BrandRepository.java
@@ -4,7 +4,9 @@ import com.chanthai.phoneshop.entity.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BrandRepository extends JpaRepository<Brand,Integer> {
-
+    List<Brand> findByNameLike(String name);
 }
