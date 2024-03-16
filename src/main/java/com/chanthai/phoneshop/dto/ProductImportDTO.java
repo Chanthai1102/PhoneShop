db1129543e2c5a723d4479e233e247c3b6cc50d1
Chanthai Thy <chanthaithy.bs2021@gmail.com>
2024-03-16 10:10:20 +0700

    not complete Sell_Product

--- src/main/java/com/chanthai/phoneshop/dto/ProductImportDTO.java
+++ src/main/java/com/chanthai/phoneshop/dto/ProductImportDTO.java
@@ -1,12 +1,14 @@
 package com.chanthai.phoneshop.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Data
 public class ProductImportDTO {
     @NotNull(message = "Product id can't be null")
@@ -19,5 +21,6 @@ public class ProductImportDTO {
     private BigDecimal importPrice;
 
     @NotNull(message = "Import Date Can't be Null")
-    private LocalDate importDate;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    private LocalDateTime importDate;
 }
